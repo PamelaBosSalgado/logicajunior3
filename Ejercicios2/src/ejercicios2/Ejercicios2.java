6744d662a7d2be0bc2919951229e3a7fdfe99c92
Pamela Bos <pame.bos@hotmail.com>
2018-06-11 17:09:43 -0300

    Tercer commit
    Ejercicios Básicos con Estructura Secuencial

---
+++ Ejercicios2/src/ejercicios2/Ejercicios2.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicios2;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Pame
+ */
+public class Ejercicios2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+       // Programa Java que lea dos números enteros por teclado y 
+        // los muestre por pantalla.
+        /*
+          int n1, n2;
+         Scanner sc = new Scanner(System.in);
+        //leer el primer número 
+        System.out.println("Introduce un número entero: "); 
+        n1 = sc.nextInt();      //lee un entero por teclado
+        //leer el segundo número
+        System.out.println("Introduce otro número entero: "); 
+        n2 = sc.nextInt();      //lee un entero por teclado
+        //mostrar resultado
+        System.out.println("Ha introducido los números: " + n1 + " y " + n2);
+        
+        
+        //Programa Java que lea un nombre y muestre por pantalla: 
+        //“Buenos dias nombre_introducido”.
+        
+        String nombre; 
+        
+        Scanner teclado = new Scanner (System.in);
+            System.out.println("Introduzca nombre: ");
+            nombre = teclado.nextLine();
+        System.out.println("Buenos dias señor " +nombre);
+
+
+        // Programa Java que lee un número entero por teclado y obtiene y 
+        // muestra por pantalla el doble y el triple de ese número.
+        
+        int num; 
+        int calcular; 
+        
+        Scanner teclado = new Scanner(System.in); 
+        System.out.println("Introduzca un número: ");
+            num = teclado.nextInt();
+            calcular = num * 2; 
+            System.out.println("El doble del número introducido es: " +calcular);
+            calcular = num * 3;
+            System.out.println("El triple del número introducido es: " + calcular);
+            
+    
+        // Programa que lea una cantidad de grados centígrados y la pase a 
+        // grados Fahrenheit. 
+        // La fórmula correspondiente es: F = 32 + ( 9 * C / 5)
+        
+        int grado;
+        int calcularF;
+        
+        Scanner teclado = new Scanner (System.in);
+        System.out.println("Introduzca grado ºC: ");
+        grado = teclado.nextInt();
+        calcularF = 32 + (9 * grado / 5);
+        System.out.println("Grado pasado a ºF: "+ calcularF);
+    
+
+        // Programa que lee por teclado el valor del radio de una 
+        // circunferencia y calcula y muestra por pantalla la longitud y el 
+        // área de la circunferencia. Longitud de la circunferencia = 
+        // 2*PI*Radio, Area de la circunferencia = PI*Radio^2  
+        
+        double radio; 
+        double calcularR;
+        double calcularA;
+        double PI = 3.14;
+        
+        Scanner teclado = new Scanner (System.in);
+        System.out.println("Introduzca numero del radio de la circunferencia: ");
+        radio = teclado.nextInt();
+        calcularR = 2 * PI * radio;
+        calcularA = Math.pow(radio, 2 );
+        System.out.println("Longitud de la circunferencia: "+ calcularR);
+        System.out.println("Area de la circunferencia: "+ calcularA);
+
+        
+        // Programa que pase una velocidad en Km/h a m/s. 
+        // La velocidad se lee por teclado.
+        
+        double nume;
+        double calcular;
+        
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Introduzca kilómetro: ");
+        nume = teclado.nextDouble();
+        calcular = nume / 0.0010000;
+        System.out.println("Velocidad pasada a 'Metros' \n m = km \n -------- \n 0.0010000 : " +calcular);
+        
+    */ 
+        
+        // Programa lea la longitud de los catetos de un triángulo 
+        // rectángulo y calcule la longitud de la hipotenusa según el 
+        // teorema de Pitágoras. h^2 = c^2(1) + c^2(2)
+        
+        
+        
+        
+        
+    }
+}
